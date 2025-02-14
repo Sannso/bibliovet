babbd1a6d75165b997915d02c779e3c30b8d4ea0
Sanss <santi-2000-12@hotmail.com>
2025-02-13 21:32:39 -0500

    :(

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
-
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact({ compat: true })],
   output: 'server',
-  adapter: vercel(),
-  prerender: { enabled: false }
+  adapter: vercel()
 });
--- global.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "flowbite/plugin";
-
-declare global {
-    interface Window {
-      initGallery: () => void;
-    }
-  }
